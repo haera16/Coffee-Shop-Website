5f835eed3860b7866ac5696831c48e7d3666ff91
Ashmita <ashmita.singha16.4@gmail.com>
2025-05-12 23:59:52 +0530

    copyright ver

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ document.querySelector('#search-icon').onclick = ()=>{
   search.classList.toggle('active');
 }
 
+let header=document.querySelector('header');
+
+window.addEventListener('scroll', ()=> {
+  header.classList.toggle('shadow', window.scrollY > 0);
+});
